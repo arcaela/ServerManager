fe0afad128e6f4d2d3f7b463a89280a19c15bf4e
Alejandro Reyes <arcaela12@gmail.com>
2020-05-20 00:26:15 -0400

    add composer installer

--- install.sh
+++ install.sh
@@ -21,3 +21,26 @@ sudo mysql -uroot <<MYSQL_SCRIPT
     ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
     FLUSH PRIVILEGES;
 MYSQL_SCRIPT
+
+echo "Instalacion de Composer: "
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+php -r "unlink('composer-setup.php');"
+if [ -d "$HOME/.config/composer/vendor/bin" ]; then
+    echo 'export PATH="$PATH:$HOME/.config/composer/vendor/bin"' >> ~/.bashrc
+    source ~/.bashrc
+fi
+
+echo "Instalacion de NodeJs: "
+VERSION=v12.16.3
+DISTRO=linux-x64
+wget https://nodejs.org/dist/$VERSION/node-$VERSION-$DISTRO.tar.xz
+sudo mkdir -p /usr/local/lib/nodejs
+sudo tar -xJvf node-$VERSION-$DISTRO.tar.xz -C /usr/local/lib/nodejs
+sudo rm -rf ./node-$VERSION-$DISTRO.tar.xz
+sudo ln -s /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin/node /usr/bin/node
+sudo ln -s /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin/npm /usr/bin/npm
+sudo ln -s /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin/npx /usr/bin/npx
+
+echo "Instalacion de Laravel: "
+composer global require laravel/installer
