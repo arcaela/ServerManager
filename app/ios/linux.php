efd50fe839e29e664771f7ecd529001e85e5d6cc
Alejandro Reyes <arcaela.reyes@gmail.com>
2020-06-11 03:39:52 -0400

    Fix

--- app/ios/linux.php
+++ app/ios/linux.php
@@ -12,7 +12,8 @@ return [
         echo Console::run("sudo a2dissite *.conf");
         echo Console::run("sudo rm -rf /etc/apache2/sites-available/*.conf");
         line(Console::run("sudo systemctl stop apache2"));
-
+        $file = store("/etc/apache2/sites-available/000-default.conf")->makeHasFile;
+        $is_done = $file->setContent("<VirtualHost *:80>\nServerAdmin admin@localhost\nDocumentRoot /var/www\n<Directory /var/www>\nOptions Indexes\nRequire all granted\n</Directory>\n</VirtualHost>");
     },
     'add'=>function($item){
         $file=store($this->path['vhosts'].$item->CONF_FILE)->unlink;
